ac1fb2167f51d0c3093abeb4d9983856570411f0
Fernando Felício <fernandofelicio.dev@gmail.com>
2024-01-03 16:12:25 -0300

    Creating Guest class

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("How many rooms will be rented?");
+		int rented;
+		
+		
+		scanner.close();
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module guest_house {
+}
